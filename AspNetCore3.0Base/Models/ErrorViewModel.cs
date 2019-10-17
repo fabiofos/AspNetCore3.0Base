e69454c000371c0b2517e20d2fbefc3f2f96315e
FabsilvaDeloitte <fabiofos@gmail.com>
2019-10-17 16:38:22 -0300

    Base Version .net core 3.0

---
+++ AspNetCore3.0Base/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCore3._0Base.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
